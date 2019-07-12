98a412236d009702625b340cab049f91d04255aa
MITSUNARI Shigeo <herumi@nifty.com>
2019-07-13 07:07:04 +0900

    remove url

--- html/test.js
+++ html/test.js
@@ -4,7 +4,7 @@ const assert = require('assert')
 const fetch = require('node-fetch')
 const ot = require('./ot.js')
 const { performance } = require('perf_hooks')
-const URL = 'https://herumi.com:50002/cgi-bin/ot.cgi'
+const URL = ''
 
 function bench (label, count, func) {
   const start = performance.now()
